efda2aa67067c148d2d1f7dde3be308abead4fd8
Precizne <63221503+Precizne@users.noreply.github.com>
2026-01-09 21:57:23 +0530

    EXP-04: RandomForest n=100 depth=15 | Standard Scaling | All Features | Test Split 0.4

--- src/train.py
+++ src/train.py
@@ -22,11 +22,11 @@ X, y = load_data(DATA_PATH)
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.4, random_state=42
 )
 
 # Preprocessing (CHANGE FLAG IF NEEDED)
-X_train, X_test = preprocess(X_train, X_test, scale=False)
+X_train, X_test = preprocess(X_train, X_test, scale=True)
 
 # Model
 model = get_model()
