a7a9482ea25d475457b412f8dd243a67d6b70d0f
Precizne <63221503+Precizne@users.noreply.github.com>
2026-01-09 21:36:57 +0530

    add(model): Add experiment 3

--- src/model.py
+++ src/model.py
@@ -5,13 +5,20 @@ def get_model():
     """
     CHANGE MODEL HERE FOR EACH EXPERIMENT
     """
-    # Example 1: Linear Regression
+    # Experiment 1: Linear Regression
     # return LinearRegression()
 
-    # Example 2: Ridge Regression
+    # Experiment 2: Ridge Regression
     # return Ridge(alpha=1.0)
 
     # Example 3: Random Forest
+    # return RandomForestRegressor(
+    #     n_estimators=50,
+    #     max_depth=15,
+    #     random_state=42
+    # )
+
+    # Example 4: Random Forest
     # return RandomForestRegressor(
     #     n_estimators=100,
     #     max_depth=15,
